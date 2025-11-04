b92f912917e978d853f6f243f8e229b1fa00706a
kirtidhwajpatra <kirtidhwajpatra@gmail.com>
2025-11-04 20:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (27.85%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -3,17 +3,16 @@ public:
     vector<string> result;
 
     void backtrack(string current, int open, int close, int n) {
-        // Base case: if the current string is of length 2*n, it’s valid
+    
         if (current.length() == 2 * n) {
             result.push_back(current);
             return;
         }
 
-        // Add '(' if we still have open brackets left
+
         if (open < n)
             backtrack(current + "(", open + 1, close, n);
 
-        // Add ')' if we can close more than we've opened
         if (close < open)
             backtrack(current + ")", open, close + 1, n);
     }
