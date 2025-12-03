e3d31f6b9e4f87b72494c0aa4f85884244d925f9
kirtidhwajpatra <kirtidhwajpatra@gmail.com>
2025-12-03 22:53:54 +0530

    Time: 2 ms (30.13%) | Memory: 52.1 MB (88.32%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach = 0;
+        for (int i = 0; i <= reach && i < nums.size(); i++) {
+            reach = max(reach, i + nums[i]);
+        }
+        return reach >= nums.size() - 1;
+    }
+};
