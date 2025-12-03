518514e51916b671136823196843b43cdc0d3f6a
kirtidhwajpatra <kirtidhwajpatra@gmail.com>
2025-12-03 22:55:57 +0530

    Time: 0 ms (100.00%) | Memory: 79.3 MB (83.36%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,9 +3,11 @@ public:
     int maxProfit(vector<int>& prices) {
         int buy1 = INT_MAX, buy2 = INT_MAX;
         int profit1 = 0, profit2 = 0;
+        
         for (int p : prices) {
             buy1 = min(buy1, p);
             profit1 = max(profit1, p - buy1);
+
             buy2 = min(buy2, p - profit1);
             profit2 = max(profit2, p - buy2);
         }
