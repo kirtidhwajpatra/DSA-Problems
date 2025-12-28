dd60e37a61a5457b408c14f4f516cb9eb3096d38
kirtidhwajpatra <kirtidhwajpatra@gmail.com>
2025-12-28 19:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (98.91%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+
+        for (int layer = 0; layer < numRows; layer++) {
+            vector<int> row(layer + 1, 1);
+
+            for (int slot = 1; slot < layer; slot++) {
+                row[slot] = triangle[layer - 1][slot - 1]
+                          + triangle[layer - 1][slot];
+            }
+
+            triangle.push_back(move(row));
+        }
+
+        return triangle;
+    }
+};
