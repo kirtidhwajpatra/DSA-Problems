5f0231f1dbe26e872d36740d85ab73e5c0c8ff90
kirtidhwajpatra <kirtidhwajpatra@gmail.com>
2025-12-30 21:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (49.08%) - LeetSync

---
+++ 553-optimal-division/optimal-division.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+        
+        int n = nums.size();
+        if(n==1) return to_string(nums[0]);
+        if(n==2) return to_string(nums[0]) + "/" + to_string(nums[1]);
+        
+          string s;
+         s += to_string(nums[0]);
+          s += "/(";
+        
+        for(int i=1;i<n;i++){
+            s += to_string(nums[i]);
+            if(i!=n-1) s += "/";
+           }
+        
+        s += ")";
+        return s;
+      }
+};
