d1cac4b1ca178e2b03b3e64d02962da33b86d1e7
kirtidhwajpatra <kirtidhwajpatra@gmail.com>
2026-01-13 22:07:20 +0530

    Time: 3 ms (9.15%) | Memory: 29.1 MB (5.42%) - LeetSync

--- 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.cpp
+++ 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.cpp
@@ -5,15 +5,18 @@ public:
         vector<long long> length(n + 1);
         length[0] = 1;
 
-        // Build lengths safely (cap at k)
+
+
         for (int i = 1; i <= n; i++) {
             length[i] = min(length[i - 1] * 2, k);
         }
 
         int shift = 0;
 
-        // Reverse traversal
+
+
         for (int i = n; i > 0; i--) {
+            
             long long half = length[i - 1];
 
             if (k > half) {
@@ -24,6 +27,9 @@ public:
             }
         }
 
+
         return char('a' + (shift % 26));
+
+
     }
 };
