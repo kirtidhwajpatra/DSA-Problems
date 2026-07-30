35f0275b5d212a91fe0eb6634fbb504891cec629
kirtidhwajpatra <kirtidhwajpatra@gmail.com>
2026-07-30 22:53:50 +0530

    Time: 69 ms (42.03%) | Memory: 111.3 MB (35.13%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,14 @@
-
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
 
-        if(set<int>(nums.begin(),nums.end()).size() < nums.size()){
-            return true;
+        unordered_set<int> st;
+         for(int i=0; i<nums.size(); i++){
+            if(st.find(nums[i]) != st.end()){
+                return true;
+            }  
+            st.insert(nums[i]);
         }
         return false;
-
     }
 };
-
-
